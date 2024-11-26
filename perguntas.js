0e38abf16cfb6cb4fa7c896802b5a6baa8779287
locatelli <167982869+pearwaz1n@users.noreply.github.com>
2024-11-26 08:10:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Hardware',
+    'O que é Placa de video?',
+    'É um componente de hardware que processa dados relacionados a imagens e as exibe em um monitor ou tela. é uma linguagem de programação'
+)
+
+criaCartao(
+    'Atalhos',
+    'Qual a funcionalidade do conjunto de teclas ctrl+alt+D?',
+    'Tem como funcionalidade a criação de outra área de trabalho'
+)
+
+criaCartao(
+    'Hardware',
+    'O que é um processador?',
+    'É a parte de um sistema computacional, que realiza as instruções de um programa de computador, que executar a aritmética básica, orientado por um padrão binário 0 e 1 que possibilita a entrada, o processamento e saída de dados'
+)
+
+criaCartao(
+    'Atalhos',
+    'Qual a funcionalidade do conjunto de teclas ctrl+shift+esc?',
+    'Nesse conjunto de teclas inicializa-se o gerenciador de tarefas.'
+)
