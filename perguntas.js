1a4062163cbe6c8ed1d1e057aa57f2d5e13681f2
locatelli <167982869+pearwaz1n@users.noreply.github.com>
2024-11-26 08:30:20 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -10,14 +10,15 @@ criaCartao(
     'Tem como funcionalidade a criação de outra área de trabalho'
 )
 
+
+criaCartao(
+    'Atalhos',
+    'Qual a Funcionalidade do conjunto de teclas ctrl+shift+esc?',
+    'Nesse conjunto de teclas inicializa-se o gerenciador de tarefas.'
+)
+
 criaCartao(
     'Hardware',
     'O que é um processador?',
     'É a parte de um sistema computacional, que realiza as instruções de um programa de computador, que executar a aritmética básica, orientado por um padrão binário 0 e 1 que possibilita a entrada, o processamento e saída de dados'
-)
-
-criaCartao(
-    'Atalhos',
-    'Qual a funcionalidade do conjunto de teclas ctrl+shift+esc?',
-    'Nesse conjunto de teclas inicializa-se o gerenciador de tarefas.'
 )
